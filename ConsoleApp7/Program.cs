04c78ca0251607a9476fa0207869b0993f61e6ac
Danila <karantinz@mail.ru>
2021-09-15 15:07:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exit = "exit";
+            string userInput;
+
+            Console.WriteLine("Вы вошли в систему, чтобы выйти введите exit");
+
+            while (true)
+            {
+                Console.WriteLine("Ждёте");
+                userInput = Convert.ToString(Console.ReadLine());
+
+                if (userInput == exit)
+                {
+                    Console.WriteLine("Вы вышли");
+                    break;
+                }
+            }
+        }
+    }
+}
